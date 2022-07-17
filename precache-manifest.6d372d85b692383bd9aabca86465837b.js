b7a9a54419482bccf80f49d1d510b3ec089a5717
noobone01 <v212prajapati@gmail.com>
2022-07-17 09:15:00 +0530

    Updates

---
+++ precache-manifest.6d372d85b692383bd9aabca86465837b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1ce04a30127fac92318",
+    "url": "/react-app/static/css/main.481e9b66.chunk.css"
+  },
+  {
+    "revision": "f1ce04a30127fac92318",
+    "url": "/react-app/static/js/main.f1ce04a3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/react-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "19f9147e2c381590d3be",
+    "url": "/react-app/static/js/runtime~main.19f9147e.js"
+  },
+  {
+    "revision": "23a3bb52fbd5c19a36c1e09dcd51e0a9",
+    "url": "/react-app/index.html"
+  }
+];
